f4751368ab67e49f9d07975022af26172e7fde64
Virginia Thayer <virginia@stripe.com>
2019-06-18 12:58:54 -0700

    initial commit. sketching out some event handler ideas.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,33 @@
-// Your code goes here
+
+// on document load, ...
+
+// when scroll header img, set border-radius to 50%
+
+
+
+// when keydown on document, set background color to background-color: #17A2B8. cancel on keyup
+
+
+
+
+// mouseover .content-destination img, change opacity
+
+
+// click any div in class .content-pick, add border: color #FFEBCD, size 2px, dashed
+
+
+
+// when click on Island Getaway (.destination:lastChild?), alert some Kokomo lyrics
+
+
+
+// 
+
+
+// 
+
+
+// 
+
+
+// 
